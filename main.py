a04894ede90cd10f6c9b4875aad9e8a33cce4b39
Carlos <21000405@alumnos.utleon.edu.mx>
2025-02-11 14:18:31 -0600

    10022025

--- main.py
+++ main.py
@@ -1,22 +1,53 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return '<h1>Hola mundo!</h1>'
+    titulo = 'IDGS801'
+    lista = ['Carlos', 'Christian', 'Briones', 'Milton']
+    return render_template('index.html', titulo=titulo, lista=lista)
 
-@app.route("/hola")
+
+@app.route("/ejemplo1")
+def ejemplo1():
+    return render_template("ejemplo1.html")
+
+@app.route("/ejemplo2")
+def ejemplo2():
+    return render_template("ejemplo2.html")
+
+@app.route('/hola')
 def hola():
-    return '<h1>modulo 2</h1>'
+    return "<h1>Hola, Mundo!</h1>"
 
-@app.route("/user/<string:user>")
+@app.route('/user/<string:user>')
 def user(user):
-    return f'<h1>Hola, {user}</h1>'
+    return f'<h1>Hola, {user}!</h1>'
 
-@app.route("/numero/<int:n>")
+@app.route('/numero/<int:n>')
 def numero(n):
-    return f'<h1>Tienes:, ${n}</h1>'
+    return f'<h1>El número es: {n}</h1>'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/user/<int:id>/<string:username>')
+def user2(id, username):
+    return f'<h1>El usuario es: {username} con id: {id}</h1>'
+
+@app.route('/suma/<float:n1>/<float:n2>')
+def suma(n1, n2):
+    return f'<h1>La suma es: {n1 + n2}</h1>'
+
+@app.route('/default/')
+@app.route('/default/<string:param>')
+def func(param='juan'):
+    return f'''
+        <form action="/default/" method="get">
+            <label for="param">Ingresa el parámetro:</label>
+            <input type="text" id="param" name="param" value="{param}">
+            <button type="submit">Enviar</button>
+        </form>
+        <h1>El parámetro es: {param}</h1>
+    '''
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000)
